6a4a9a00b923c6506101f82ae3dd7d2b636d67bf
Soban Mahmood <sobanmahmood@gmail.com>
2023-02-28 03:04:10 +0500

    Add script for database and table creation

---
+++ transaction-service/transaction_db.sql
@@ -0,0 +1,17 @@
+drop database transactiondb;
+drop user transaction;
+
+create user transaction with password 'password';
+create database transactiondb with template=template0 owner=transaction;
+\connect transactiondb;
+alter default privileges grant all on tables to transaction;
+alter default privileges grant all on sequences to transaction;
+
+create table transactions(
+transaction_id bigserial primary key not null,
+customer_id integer not null,
+amount numeric(10,2) not null,
+transaction_date timestamp not null
+);
+
+insert into transactions (customer_id, amount, transaction_date) values (1, 100, date '2023-01-09');
