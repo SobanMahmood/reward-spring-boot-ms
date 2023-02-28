3b0c9ee57624e32bf3f01011f6d22d7a4e25df38
Soban Mahmood <sobanmahmood@gmail.com>
2023-02-28 09:42:22 +0500

    Add CrossOrigin in reward controller to work with react

--- reward_service/src/main/java/com/reward/controller/RewardController.java
+++ reward_service/src/main/java/com/reward/controller/RewardController.java
@@ -3,6 +3,7 @@ package com.reward.controller;
 import com.reward.model.RewardPoints;
 import com.reward.service.RewardService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +17,7 @@ public class RewardController {
     @Autowired
     private RewardService rewardService;
 
+    @CrossOrigin
     @RequestMapping("/customer/{customerId}")
     public RewardPoints getReward(@PathVariable("customerId") Integer customerId) {
         // TODO: replace the dummy data with original data
