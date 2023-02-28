5a236696d44931bc08197fd3b1e3863957ef131f
Soban Mahmood <sobanmahmood@gmail.com>
2023-02-28 11:12:20 +0500

    Update UI, add home layout

--- reward-web/src/App.js
+++ reward-web/src/App.js
@@ -2,40 +2,49 @@ import logo from './logo.svg';
 import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
+import Layout from './component/Layout';
+import {Routes, Route} from 'react-router-dom';
+import Home from './component/home/Home';
 
 function App() {
+  const [rewards, setRewards] = useState();
   const [reward, setReward] = useState();
  
-  const getReward = async() =>{
+  const getRewards = async() =>{
     try {
       const response = await api.get("/reward/customer/1");
-      setReward(response.data);
+      setRewards(response.data);
     }
     catch(err){
       console.log(err);
     }
   }
 
+  const getRewardData = async (cutomerId) => {
+    try {
+      const response = await api.get('reward/customer/${customerId}');
+      const singleReward = response.data;
+      setReward(singleReward);
+    }
+    catch(err)
+    {
+
+    }
+  }
+
   useEffect(()=> {
-    getReward();
+    getRewards();
   },[])
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route path="/" element={<Home/>}></Route>
+        </Route>
+      </Routes>
+
     </div>
   );
 }
